7beb3f6d34f7ee92d6c2c905f6755031f871987e
onifade <onifadeololadefavor1@gmail.com>
2024-08-12 17:30:21 +0100

    tiredsucess

--- server/Routes/userRoutes.js
+++ server/Routes/userRoutes.js
@@ -1,7 +1,6 @@
 const express = require("express")
 const { posted } = require("../controller/User")
 const router = express.Router()
-
 router.post("/post", posted)
 router.get("/",(req,res)=>{
         res.json({message:"welcome to backend"})
