c09b356418fc3d4290e829a9ee4a83db9d581f1a
Onifade Ololade Favour <115437722+Favour123@users.noreply.github.com>
2024-08-12 16:17:57 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,7 +6,13 @@ const  {router} = require("./Routes/userRoutes");
 const { connectDB } = require("./model/db");
 const PORT = process.env.PORT || 5000;
 connectDB()
-app.use(cors())
+app.use(cors(
+   {
+      origin:(lectplus-menserver.vercel.app),
+      method:("POST","GET"),
+      credential:true
+   }
+))
 app.use(express.json());
 app.listen(PORT, () => {
    console.log(`you are running on ${PORT}`);
