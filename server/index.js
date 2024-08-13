0e775c7d88992d5dcfbeaa57ef9b7d72e5ad9f60
onifade <onifadeololadefavor1@gmail.com>
2024-08-14 00:09:40 +0100

    worked

--- server/index.js
+++ server/index.js
@@ -6,13 +6,11 @@ const  {router} = require("./Routes/userRoutes");
 const { connectDB } = require("./model/db");
 const PORT = process.env.PORT || 5000;
 connectDB()
-app.use(cors(
-   {
-      origin:["(https://lectplus-men.vercel.app)"],
-      method:["POST","GET"],
-      credential:true
-   }
-))
+app.use(cors({
+   origin: ["https://lectplus-men.vercel.app"], // The URL of your frontend
+   methods: ["POST", "GET"], // Allowed methods
+   credentials: true // Allow credentials like cookies to be sent
+ }));
 app.use(express.json());
 app.listen(PORT, () => {
    console.log(`you are running on ${PORT}`);
